a78ba1a20a8f0c3dc273b21b4229c6f0899404f2
Samreen Ayesha <samreenayesja2@gmail.com>
2023-01-21 17:04:57 +0530

    initial commit

---
+++ lib/views/dummypage.dart
@@ -0,0 +1,17 @@
+class name extends StatefulWidget {
+  final String value;
+
+  name({Key? key}) : super(key: key);
+  
+  @override
+  _nameState createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
